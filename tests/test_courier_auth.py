297afccfa3897a0fdc3d65a3902f0eb462297b64
Albert Shadzhanov <albertm79@yandex.ru>
2025-05-25 21:36:02 +0300

    Added testing for tests/test_courier_auth.py

--- tests/test_courier_auth.py
+++ tests/test_courier_auth.py
@@ -3,24 +3,43 @@ import pytest
 import allure
 import random
 from data import Url
+from data import Data
 
 
 
-
-@allure.title('Успешная авторизация курьера')
+@allure.title('Авторизация курьера')
 class TestLoginCourier:
-    def test_login_courier_success(self, registered_courier, BASE_URL=None):
-        response = requests.post(BASE_URL + 'api/v1/courier/login', json={
+    @allure.step("Успешная авторизация")
+    def test_login_courier_success(self, registered_courier):
+        response = requests.post(f'{Url.BASE_URL}{Url.LOGIN_URL}', json={
             "login": registered_courier["login"],
             "password": registered_courier["password"]
         })
         assert response.status_code == 200
         assert "id" in response.json()
 
-    @allure.title('Авторизация с неверным паролем')
+    @allure.step("Авторизация с неверным паролем")
     def test_login_wrong_password(self, registered_courier):
-        response = requests.post(API_URL + '/courier/login', json={
+        response = requests.post(f'{Url.BASE_URL}{Url.LOGIN_URL}', json={
             "login": registered_courier["login"],
-            "password": "wrong_password"
+            "password": "один"
         })
         assert response.status_code == 404
+
+    @allure.step("Авторизация с неверными данными")
+    @pytest.mark.parametrize("invalid_data",Data.INVALID_TEST_DATA)
+    def test_login_invalid_credentials(self, invalid_data):
+        response = requests.post(f'{Url.BASE_URL}{Url.LOGIN_URL}', json=invalid_data)
+        assert response.status_code == 404
+
+    @allure.step("Авторизация без обязательных полей")
+    @pytest.mark.parametrize("missing_field", ["login", "password"])
+    def test_login_missing_fields(self, registered_courier, missing_field):
+        data = {
+            "login": registered_courier["login"],
+            "password": registered_courier["password"]
+        }
+        del data[missing_field]
+
+        response = requests.post(f'{Url.BASE_URL}{Url.LOGIN_URL}', json=data)
+        assert response.status_code == 400
